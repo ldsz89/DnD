f8fae00035936a559be9f2079c364f61074207f8
Ubuntu <ubuntu@ip-172-31-59-139.ec2.internal>
2017-05-25 16:32:00 +0000

    Changing templates and adding character creator

--- dnd/urls.py
+++ dnd/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^home/', include('home.urls')),
+    url(r'^ccreator/', include('ccreator.urls')),
     url(r'^admin/', admin.site.urls),
 ]
