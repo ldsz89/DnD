b1595587d098349ffa05aad1982b152285a6e6b2
Ubuntu <ubuntu@ip-172-31-59-139.ec2.internal>
2017-05-29 03:04:42 +0000

    Allow null for new characters and self reference

--- ccreator/models.py
+++ ccreator/models.py
@@ -11,59 +11,77 @@ class Character(models.Model):
     race = models.CharField(max_length=50)
     background = models.CharField(max_length=50)
     alignment = models.CharField(max_length=50)
-    strength = models.IntegerField(blank=True)
-    dexterity = models.IntegerField(blank=True)
-    constitution = models.IntegerField(blank=True)
-    intelligence = models.IntegerField(blank=True)
-    wisdom = models.IntegerField(blank=True)
-    charisma = models.IntegerField(blank=True)
-    acrobatics = models.IntegerField(blank=True)
-    animal_handling = models.IntegerField(blank=True)
-    arcana = models.IntegerField(blank=True)
-    athletics = models.IntegerField(blank=True)
-    deception = models.IntegerField(blank=True)
-    history = models.IntegerField(blank=True)
-    insight = models.IntegerField(blank=True)
-    intimidation = models.IntegerField(blank=True)
-    investigation = models.IntegerField(blank=True)
-    medicine = models.IntegerField(blank=True)
-    nature = models.IntegerField(blank=True)
-    perception = models.IntegerField(blank=True)
-    performance = models.IntegerField(blank=True)
-    persuasion = models.IntegerField(blank=True)
-    religion = models.IntegerField(blank=True)
-    sleight_of_hand = models.IntegerField(blank=True)
-    stealth = models.IntegerField(blank=True)
-    survival = models.IntegerField(blank=True)
-    armor_class = models.IntegerField(blank=True)
-    speed = models.IntegerField(blank=True)
-    hp = models.IntegerField(blank=True)
-    personality = models.IntegerField(blank=True)
-    ideals = models.IntegerField(blank=True)
-    bonds = models.IntegerField(blank=True)
-    flaws = models.IntegerField(blank=True)
+    strength = models.IntegerField(null=True)
+    dexterity = models.IntegerField(null=True)
+    constitution = models.IntegerField(null=True)
+    intelligence = models.IntegerField(null=True)
+    wisdom = models.IntegerField(null=True)
+    charisma = models.IntegerField(null=True)
+    acrobatics = models.IntegerField(null=True)
+    animal_handling = models.IntegerField(null=True)
+    arcana = models.IntegerField(null=True)
+    athletics = models.IntegerField(null=True)
+    deception = models.IntegerField(null=True)
+    history = models.IntegerField(null=True)
+    insight = models.IntegerField(null=True)
+    intimidation = models.IntegerField(null=True)
+    investigation = models.IntegerField(null=True)
+    medicine = models.IntegerField(null=True)
+    nature = models.IntegerField(null=True)
+    perception = models.IntegerField(null=True)
+    performance = models.IntegerField(null=True)
+    persuasion = models.IntegerField(null=True)
+    religion = models.IntegerField(null=True)
+    sleight_of_hand = models.IntegerField(null=True)
+    stealth = models.IntegerField(null=True)
+    survival = models.IntegerField(null=True)
+    armor_class = models.IntegerField(null=True)
+    speed = models.IntegerField(null=True)
+    hp = models.IntegerField(null=True)
+    personality = models.IntegerField(null=True)
+    ideals = models.IntegerField(null=True)
+    bonds = models.IntegerField(null=True)
+    flaws = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
 
 class CharacterFeatures(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     url = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Traits(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     url = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 class Languages(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     url = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Equipment(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     url = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Spells(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     url = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
