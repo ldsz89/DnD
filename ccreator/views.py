35d8155cc78c22a814cb8fff5a154886b59f728c
Ubuntu <ubuntu@ip-172-31-59-139.ec2.internal>
2017-07-19 18:46:35 +0000

    JSON working with displaying classes

--- ccreator/views.py
+++ ccreator/views.py
@@ -27,11 +27,7 @@ class CharacterCreate(CreateView):
     def get_context_data(self, **kwargs):
         context = super(CreateView, self).get_context_data(**kwargs)
         data = requests.get('http://dnd5eapi.co/api/classes/')
-        classes = []
-        for c in data.json()['results']:
-            classes.append(c['name'])
-        # context['classes'] = response['results']
-        context['classes'] = classes
+        context['classes'] = data.text
         return context
 
 class CharacterUpdate(UpdateView):
